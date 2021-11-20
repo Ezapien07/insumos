82d069b4467ce12d6486f192e30b4079cfe7e562
Ezapien07 <zapienernesto5@gmail.com>
2021-11-20 01:25:44 -0600

    =bases

---
+++ app/Models/Insumos.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+
+class Insumos extends Model
+{
+    use HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'id',
+        'nombre',
+        'descripcion',
+        'codigo',
+        'cantidad',
+        'estatus'
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+    ];
+}
